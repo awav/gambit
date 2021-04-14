bc6302732f75a35744e047b2e8aedf2a951ff53f
Tilman Roeder <dyed.green.info@gmail.com>
2021-04-14 19:58:45 +0100

    Try gradient optimization -- IT WORKS!!!!!!

--- xla_playground.py
+++ xla_playground.py
@@ -35,15 +35,68 @@ def no_jit_op(x, y, z):
     xyz = xy @ z
     return xyz
 
-def main():
+@tf.function(experimental_compile=True)
+def more_complex_graph(x: Tensor, y: Tensor, z: Tensor) -> Tensor:
+    a = x + y - z
+    b = 3 * z
+    c = tf.math.exp(y) - tf.math.log(tf.math.abs(x) + 0.001)
+    ab = a @ tf.transpose(b)
+    abc = ab @ c
+    return abc / 5
+
+# This doesn't work; the generated HLO for forward now explicitly requests to
+# get the intermediate results, which breaks the optimization
+def with_gradients():
     n, m = 1000, 2
     x = tf.random.normal((n, m))
     y = tf.random.normal((n, m))
     z = tf.random.normal((n, m))
-    res = op(x, y, z)
-    print(f"Op result = {res.numpy()}")
-    print(f"Delta = {(res - no_jit_op(x, y, z)).numpy()}")
+    with tf.autodiff.GradientTape(persistent=True) as g:
+        g.watch(x)
+        g.watch(y)
+        g.watch(z)
+        xyz = op(x, y, z)
+    dx = g.gradient(xyz, x)
+    # dy = g.gradient(xyz, y)
+    # dz = g.gradient(xyz, z)
+    return dx #, dy, dz
 
+@tf.function(experimental_compile=True)
+def gradients_full(x: Tensor, y: Tensor, z: Tensor) -> Tensor:
+    with tf.autodiff.GradientTape(persistent=True) as g:
+        g.watch(x)
+        g.watch(y)
+        g.watch(z)
+
+        xy = x @ tf.transpose(y)
+        xyz = xy @ z
+
+    dx = g.gradient(xyz, x)
+    dy = g.gradient(xyz, y)
+    dz = g.gradient(xyz, z)
+    return (xyz, dx, dy, dz)
+
+def main(simple=True):
+    n, m = 1000, 2
+    x = tf.random.normal((n, m))
+    y = tf.random.normal((n, m))
+    z = tf.random.normal((n, m))
+    if simple:
+        res = op(x, y, z)
+        print(f"Op result = {res.numpy()}")
+        print(f"Delta = {(res - no_jit_op(x, y, z)).numpy()}")
+    else:
+        res = more_complex_graph(x, y, z)
+        print(f"Op result = {res.numpy()}")
+
+def main_full_grad():
+    n, m = 1000, 2
+    x = tf.random.normal((n, m))
+    y = tf.random.normal((n, m))
+    z = tf.random.normal((n, m))
+    xyz, dx, dy, dz = gradients_full(x, y, z)
 
 if __name__ == "__main__":
-    main()
+    # main(False)
+    # with_gradients()
+    main_full_grad()
