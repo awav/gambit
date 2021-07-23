b9682b81e93f4387dc29b7856af8c07ca33be6f5
Tilman Roeder <dyed.green.info@gmail.com>
2021-07-23 15:46:58 +0100

    Update tlf

--- xla_playground.py
+++ xla_playground.py
@@ -3,26 +3,28 @@ import tensorflow as tf
 
 Tensor = TypeVar("Tensor", bound=tf.Tensor)
 
-# @tf.function(experimental_compile=True)
-# def computation(a: Tensor, b: Tensor, tri_mat: Tensor, c: Tensor) -> Tensor:
-#     outer = a @ tf.transpose(b)
-#     solve = tf.linalg.triangular_solve(tri_mat, outer)
-#     return solve @ c
-
-# large_size = 1000
-# a = tf.random.normal((100, 10))
-# b = tf.random.normal((large_size, 10))
-# tri_mat = tf.random.uniform((100, 100))
-# c = tf.random.normal((large_size, 1))
-
-# computation(a, b, tri_mat, c)
-
 @tf.function(experimental_compile=True)
-def outer_inner_add(a: Tensor, b: Tensor, c: Tensor) -> Tensor:
-    ab = a @ b
-    return tf.math.exp(ab) @ c
+def computation(a: Tensor, b: Tensor, tri_mat: Tensor, c: Tensor) -> Tensor:
+    outer = a @ tf.transpose(b)
+    solve = tf.linalg.triangular_solve(tri_mat, outer)
+    return solve @ c
 
-a = tf.random.normal((997, 10))
-b = tf.random.normal((10, 100000))
-c = tf.random.normal((100000, 1))
-outer_inner_add(a, b, c)
+large_size = 100000
+a = tf.random.normal((1000, 10))
+b = tf.random.normal((large_size, 10))
+tri_mat = tf.random.uniform((1000, 1000))
+c = tf.random.normal((large_size, 1))
+
+computation(a, b, tri_mat, c)
+
+# print(diff.numpy())
+
+# @tf.function(experimental_compile=True)
+# def outer_inner_add(a: Tensor, b: Tensor, c: Tensor) -> Tensor:
+#     ab = a @ b
+#     return tf.math.exp(ab) @ c
+
+# a = tf.random.normal((997, 10))
+# b = tf.random.normal((10, 100000))
+# c = tf.random.normal((100000, 1))
+# outer_inner_add(a, b, c)
